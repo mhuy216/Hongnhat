71282a9ee8d84316db5d3427e2537dc6925db028
mhuy216 <163323197+mhuy216@users.noreply.github.com>
2024-03-25 23:24:35 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Hồng Nhạt';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Hồng Nhạt';
+const contentCard = 'Chúc mừng sinh nhật Hồng Nhạt. Chúc Hồng Nhạt luôn xinh đẹp, dễ thương, vui vẻ, học giỏi và khỏe mạnh nè. Sang tuổi 18 gặp nhiều điều may mắn, hong có lì nữa, phải ngoan ngoãn và đặc biệt là đạt kết quả cao trong kì thì THPT QG. Và cuối cùng là chúc Hồng Nhạt tất cả:33333';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'z5249540815189_be5dffba3c5107844f426146888c75de.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
